8238b51fa2f45022fc974e1c09b854860ac045cc
Aroslav Kochkin <kochkin.2328@inbox.ru>
2024-11-18 20:45:37 +0700

    commit-2

---
+++ Examination2App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Examination2App
+//
+//  Created by Ярослав Кочкин on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .white
+	}
+
+}
+
